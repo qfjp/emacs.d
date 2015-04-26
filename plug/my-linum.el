fefd3b9e9dc7fc1b6710ee9a1ec5855a00951af1
DJP <djpade@gmail.com>
2015-04-26 15:40:25 -0400

    Linum shows current line number, more space between text and line number column

--- plug/my-linum.el
+++ plug/my-linum.el
@@ -5,6 +5,8 @@
   :ensure t
   :demand linum-relative
   :config
+  (setq linum-relative-current-symbol "")
+  (setq linum-relative-format "%3s ")
   (add-hook 'after-change-major-mode-hook 'linum-mode))
 (provide 'my-linum)
 ;;; my-linum ends here
