42243e3ce51879e14427903c080dbc8b9d09db0e
DJP <djpade@gmail.com>
2015-04-30 15:18:00 -0400

    Added exclusion rules to swbuff

--- plug/my-swbuff-x.el
+++ plug/my-swbuff-x.el
@@ -3,7 +3,9 @@
 ;;; Code:
 (use-package swbuff-x
   :ensure t
-  :demand swbuff-x)
+  :demand swbuff-x
+  :config
+  (setq swbuff-exclude-buffer-regexps '("^ " "^\*.*\*")))
 
 (provide 'my-swbuff-x)
 ;;; my-swbuff-x ends here
