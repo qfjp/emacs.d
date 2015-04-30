276e8ca57bbd6d8672c5ea2fd3059768567ad38c
DJP <djpade@gmail.com>
2015-04-30 01:02:16 -0400

    Added git mode-line plugin for optional state section

---
+++ plug/modeline/git-modeline.el
@@ -0,0 +1,31 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+(defvar-local git-branch-msg ""
+  "The message to display in the mode line.")
+
+(defvar-local git-branch ""
+  "The name of the current git-branch.")
+
+(defun get-git-branch ()
+  "Get the name of the current git branch (or nil if not versioned)."
+  (cond
+   ((equalp (call-process-shell-command "git rev-parse --git-dir") 0)
+    (set-branch-name)
+    (setq git-branch-msg
+          (concat
+           "  "
+           git-branch
+           " "))))
+  git-branch-msg)
+
+(defvar-local git-branch/buffer nil)
+(defun set-branch-name ()
+  "Set the branch name."
+  (setq git-branch
+        (substring
+         (shell-command-to-string "git rev-parse --abbrev-ref HEAD")
+         0 -1)))
+
+(provide 'git-modeline)
+;;; git-modeline.el ends here
