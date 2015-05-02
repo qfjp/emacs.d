2e855176b9d22a57b23d554f7064d090a8dadcff
DJP <djpade@gmail.com>
2015-05-02 18:00:06 -0400

    Added gitflow to magit (can't turn on?)

--- plug/my-magit.el
+++ plug/my-magit.el
@@ -4,12 +4,15 @@
 ;; Magit
 (use-package magit
   :ensure t
+  :ensure magit-gitflow
   :demand magit
+  :demand magit-gitflow
   :demand evil
   :config
-  (progn
-    (setq magit-auto-revert-mode nil)
-    (setq magit-last-seen-setup-instructions "1.4.0")))
+  (setq magit-auto-revert-mode nil)
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  ;;(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+  )
 
 (provide 'my-magit)
 ;;; my-magit ends here
