71c9ee5edae91e76e6f2c3ea3d8f1839948146f5
DJP <djpade@gmail.com>
2015-05-03 02:22:47 -0400

    Refactored into separate 'make-section' methods, more consistent var names

--- plug/modeline/my-mode-line.el
+++ plug/modeline/my-mode-line.el
@@ -20,108 +20,50 @@
 (defvar insert-color)
 (defvar visual-color)
 (defvar replace-color)
-(defvar cur-state-color)
-(defvar text-color)
-(defvar main-foreground)
+(defvar cur-state-bg)
+(defvar state-fg)
+(defvar main-fg)
+(defvar main-bg)
+(setq main-bg "#2f2f2f")
 
 ;; colors
-(defvar optional-normal-bg)
-(setq optional-normal-bg "#454545")
-(defvar optional-dark-bg)
-(setq optional-dark-bg "black")
+(defvar secondary-normal-bg)
+(setq secondary-normal-bg "#454545")
+(defvar secondary-dark-bg)
+(setq secondary-dark-bg "black")
+(defvar secondary-fg)
+(setq secondary-fg "black")
 
-(defvar main-section-back-color)
-(setq main-section-back-color "#2f2f2f")
 
-(defvar optional-section-back-color)
-(setq optional-section-back-color optional-normal-bg)
+(defvar secondary-bg)
+(setq secondary-bg secondary-normal-bg)
 
-(defvar normal-face)
-(defvar insert-face)
-(defvar visual-face)
-(defvar replace-face)
-(defvar optional-face)
+(defvar secondary-face)
 
 (defun refresh-colors ()
   "Set colors based on whether we are in a GUI or tty."
-  (setq text-color "gray12")
+  (setq state-fg "gray12")
   (setq normal-color "blue")
   (setq insert-color "green")
   (setq visual-color "orange")
   (setq replace-color "red")
-  (setq cur-state-color "blue")
-  (setq main-foreground "gray96")
+
+  (setq cur-state-bg "blue")
+  (setq main-fg "gray96")
   (if window-system
       (progn
         (setq normal-color "#6a95b5")
         (setq insert-color "#99cc99")
         (setq visual-color "#d28445")
-        (setq replace-color "#ac4142"))))
-
-(defun refresh-faces ()
-  "Refresh the faces for the evil state."
-  (cond
-   ((or (evil-insert-state-p) (evil-visual-state-p))
-    (setq optional-section-back-color optional-dark-bg)
-    (setq optional-face
-          `(:foreground
-            ,cur-state-color
-            :background
-            ,optional-section-back-color
-            :height
-            0.9)))
-   (t
-    (setq optional-section-back-color optional-normal-bg)
-    (setq optional-face
-          `(:foreground
-            ,main-foreground
-            :background
-            ,optional-section-back-color
-            :height
-            0.9))))
-
-  (setq normal-face
-        `(:foreground
-          ,text-color
-          :background
-          ,normal-color
-          :height
-          0.9
-          :weight bold))
-  (setq insert-face
-        `(:foreground
-          ,text-color
-          :background
-          ,insert-color
-          :height
-          0.9
-          :weight
-          bold))
-  (setq visual-face
-        `(:foreground
-          ,text-color
-          :background
-          ,visual-color
-          :height
-          0.9
-          :weight
-          bold))
-  (setq replace-face
-        `(:foreground
-          ,text-color
-          :background
-          ,replace-color
-          :height
-          0.9
-          :weight
-          bold)))
+        (setq replace-color "#ac4142")
+        (setq cur-state-bg normal-color)
+        )))
 
 (refresh-colors)
-(refresh-faces)
 
 ;; Mode line info vars
 (defvar evil-state-msg)
-(defvar optional-state-msg)
+(defvar secondary-state-msg)
 (defvar buffer-name-msg)
 (defvar major-mode-msg nil
   "The major mode string to display.")
@@ -134,56 +76,71 @@
 (defvar scrollbar-msg ""
   "The scrollbar text widget.")
 
-;; Indicate the state of the last separator
-(require 'text-scroll-bar)
+(defun airline/create-left-section (text fg bg nextbg &optional face-args)
+  "Create a section with TEXT with colors FG/BG.
+This will be a section on the left of the status bar."
+  (concat
+   (propertize
+    (concat " " text " ")
+    'face
+    (append `(:foreground ,fg :background ,bg) face-args))
+   (propertize (char-to-string ?\ue0b0)
+               'face `(:foreground ,bg :background ,nextbg))))
 
-;; Main hook
+(defun airline/create-right-section (text fg bg prevbg &optional face-args)
+  "Create a section with TEXT with colors FG/BG.
+This will be a section on the left of the status bar."
+  (concat
+   (propertize (char-to-string ?\ue0b2)
+               'face `(:foreground ,bg :background ,prevbg))
+   (propertize
+    (concat " " text " ")
+    'face
+    (append `(:foreground ,fg :background ,bg) face-args))))
+
+;; Refresh state
 (defun refresh-evil-state-msg ()
   "Return the state of evil mode."
-  (refresh-colors)
-  (refresh-faces)
   (cond
    ((evil-normal-state-p)
-    (setq cur-state-color normal-color)
+    (setq secondary-bg secondary-normal-bg)
+    (setq cur-state-bg normal-color)
+    (setq secondary-fg main-fg)
     (setq evil-state-msg
-          (concat (propertize " NORMAL " 'face normal-face)
-                  (make-right-separator
-                   optional-section-back-color normal-color))))
+          (airline/create-left-section
+           "NORMAL" state-fg normal-color secondary-bg
+           '(:weight bold))))
    ((evil-insert-state-p)
-    (setq cur-state-color insert-color)
+    (setq secondary-bg secondary-dark-bg)
+    (setq cur-state-bg insert-color)
+    (setq secondary-fg cur-state-bg)
     (setq evil-state-msg
-          (concat (propertize " INSERT " 'face insert-face)
-                  (make-right-separator
-                   optional-section-back-color insert-color))))
+          (airline/create-left-section
+           "INSERT" state-fg insert-color secondary-bg
+           '(:weight bold))))
    ((evil-visual-state-p)
-    (setq cur-state-color visual-color)
+    (setq secondary-bg secondary-dark-bg)
+    (setq cur-state-bg visual-color)
+    (setq secondary-fg cur-state-bg)
     (setq evil-state-msg
-          (concat (propertize " VISUAL " 'face visual-face)
-                  (make-right-separator
-                   optional-section-back-color visual-color))))
+          (airline/create-left-section
+           "VISUAL" state-fg visual-color secondary-bg
+           '(:weight bold))))
    ((evil-replace-state-p)
-    (setq cur-state-color replace-color)
+    (setq secondary-bg secondary-normal-bg)
+    (setq cur-state-bg replace-color)
     (setq evil-state-msg
-          (concat (propertize " REPLACE " 'face replace-face)
-                  (make-right-separator
-                   optional-section-back-color replace-color)))))
-  (refresh-faces)
-  (refresh-optional-state-msg)
-  (refresh-buffer-name-msg)
-  (refresh-major-mode-msg)
-  (refresh-encoding-msg)
-  (refresh-ruler-msg)
-  (refresh-left-right-buffer))
+          (airline/create-left-section
+           "REPLACE" state-fg replace-color secondary-bg
+           '(:weight bold)))))
+  )
 
 (defun refresh-major-mode-msg ()
   "Give an indicator for the major mode."
   (setq
    major-mode-msg
-   (concat
-    (propertize (concat "  " mode-name " ")
-                'face `(:background ,main-section-back-color))
-    (make-left-separator
-     main-section-back-color optional-section-back-color))))
+   (airline/create-right-section
+     mode-name main-fg main-bg main-bg)))
 
 (defun refresh-buffer-name-msg ()
   "Give an indicator for the current file."
@@ -191,29 +148,17 @@
         (concat
          (propertize " "
                      'face
-                     `(:background ,main-section-back-color))
+                     `(:background ,main-bg))
          (propertize " %b " 'face '(:background "black")))))
-
-(defun make-right-separator (bg fg)
-  "Given colors BG and FG, make the appropriate separator."
-  (propertize (char-to-string ?\ue0b0)
-              'face `(:foreground ,fg :background ,bg)))
-
-(defun make-left-separator (bg fg)
-  "Given a FACE plist, make the appropriate separator."
-  (propertize (char-to-string ?\ue0b2)
-              'face `(:foreground ,fg :background ,bg)))
 
 (defun refresh-encoding-msg ()
   "Return the encoding section."
   (setq encoding-msg
-        (concat
-         (propertize
-          (concat " " (symbol-name buffer-file-coding-system) " ")
-          'face
-          optional-face)
-         (make-left-separator
-          optional-section-back-color cur-state-color))))
+        (airline/create-right-section
+         (symbol-name buffer-file-coding-system)
+         secondary-fg
+         secondary-bg
+         main-bg)))
 
 (defun refresh-ruler-msg ()
   "Set the value of the ruler message."
@@ -222,43 +167,29 @@
   (setq-default nyan-wavy-trail t)
   (setq ruler-msg
         (concat
-         (propertize " " 'face `(:background
-                                 ,cur-state-color
-                                 :foreground
-                                 ,text-color))
-         ;;(sml-modeline-create)
-         (propertize (nyan-create) 'face `(:background
-                                           ,cur-state-color
-                                           :foreground
-                                           ,text-color))
-         (propertize "   "
-                     'face `(:background
-                             ,cur-state-color
-                             :foreground
-                             ,text-color))
-         (propertize "%3l:" 'face `(:background
-                                    ,cur-state-color
-                                    :foreground
-                                    ,text-color
-                                    :weight
-                                    bold))
-         (propertize "%3c " 'face `(:background
-                                    ,cur-state-color
-                                    :foreground
-                                    ,text-color)))))
+         " " (nyan-create) "  %3l: %3c "))
+  (setq ruler-msg (airline/create-right-section
+                   ruler-msg
+                   state-fg
+                   cur-state-bg
+                   secondary-bg)))
 
-(defun refresh-optional-state-msg ()
-  "Return an optional second state."
-  (setq optional-state-msg
-        (concat
-         (propertize (get-git-branch) 'face
-                     optional-face)
-         (make-right-separator
-          "gray19" optional-section-back-color))))
+(defun refresh-secondary-state-msg ()
+  "Return an secondary second state."
+  (setq secondary-state-msg
+        (airline/create-left-section (get-git-branch)
+                                     secondary-fg
+                                     secondary-bg
+                                     main-bg)))
 
 (defadvice evil-refresh-mode-line (after refresh-evil-state-msg activate)
-    "Update the indicator for the modeline state."
-    (refresh-evil-state-msg))
+  "Update the indicator for the modeline state."
+  (refresh-evil-state-msg)
+  (refresh-secondary-state-msg)
+  (refresh-buffer-name-msg)
+  (refresh-major-mode-msg)
+  (refresh-encoding-msg)
+  (refresh-ruler-msg))
 
 (defvar mode-line-left "" "The left mode line message.")
 (defvar mode-line-right "" "The right mode line message.")
@@ -277,15 +208,6 @@
      ml/fudge-val ;; quick hack to fill out buffer in console
      ))
   num-spaces)
-  ;;(cond
-  ;; ((< num-spaces 0)
-  ;;  (progn
-  ;;    (setq truncate-depth (+ 1 truncate-depth))))
-  ;; ((> num-spaces 10)
-  ;;  (progn
-  ;;    (setq truncate-depth (- 1 truncate-depth))) ;; TODO needs to be more specific
-  ;;  )
-  ;; (t num-spaces)))
 
 (defun refresh-left-msg ()
   "Set the left portion of the mode-line."
@@ -293,7 +215,7 @@
    mode-line-left
    (list
     '(:eval evil-state-msg)
-    '(:eval optional-state-msg)
+    '(:eval secondary-state-msg)
     '(:eval buffer-name-msg))))
 
 (defun refresh-right-msg ()
@@ -321,7 +243,14 @@
 
 (defun refresh-mode-line (&optional arg arg2 arg3)
   "Refresh the whole mode line; ARG, ARG2 and ARG3 provided to satisfy scroll hooks."
+  (refresh-colors)
   (refresh-evil-state-msg)
+  (refresh-secondary-state-msg)
+  (refresh-buffer-name-msg)
+  (refresh-major-mode-msg)
+  (refresh-encoding-msg)
+  (refresh-ruler-msg)
+
   (refresh-left-msg)
   (refresh-right-msg)
   (refresh-left-right-buffer))
@@ -330,7 +259,7 @@
  mode-line-format
  (list
   '(:eval mode-line-left)
-  '(:eval (propertize left-right-buffer 'face `(:background ,main-section-back-color)))
+  '(:eval (propertize left-right-buffer 'face `(:background ,main-bg)))
   '(:eval mode-line-right)))
 
 (advice-add 'evil-next-line :after
