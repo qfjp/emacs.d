7fc8bdcf42d793868701182e7bc7c3c4ee311b88
DJP <djpade@gmail.com>
2015-05-04 10:45:02 -0400

    Auxtex wrapped in use-package

--- init.el
+++ init.el
@@ -122,13 +122,16 @@
 ;; Guide key
 
 ;; Auctex
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq-default TeX-master nil)
-(setq TeX-view-program-selection
-      '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-      '(("PDF Viewer" "zathura %o")))
+(use-package tex-site
+  :ensure auctex
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
+  (setq TeX-view-program-selection
+        '((output-pdf "PDF Viewer")))
+  (setq TeX-view-program-list
+        '(("PDF Viewer" "zathura %o"))))
 
 (use-package key-chord
   :ensure t
