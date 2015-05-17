ba18eb71b56c9cad5c3a1f06a087306fca75872f
DJP <djpade@gmail.com>
2015-05-17 12:14:22 -0400

    Added keybinding for magit-refresh

--- config/keymaps/magit.el
+++ config/keymaps/magit.el
@@ -5,29 +5,33 @@
   "Evil keymaps for magit"
   (eval-after-load 'magit
     '(progn
-      (evil-set-initial-state 'magit-mode 'normal)
-      (evil-set-initial-state 'magit-status-mode 'normal)
-      (evil-set-initial-state 'magit-diff-mode 'normal)
-      (evil-set-initial-state 'magit-log-mode 'normal)
-      (evil-set-initial-state 'magit-process-mode 'normal)
-      (evil-define-key 'normal magit-mode-map
-        (kbd "-") 'magit-diff-smaller-hunks
-        (kbd "+") 'magit-diff-larger-hunks
-        (kbd "j") 'magit-goto-next-section
-        (kbd "k") 'magit-goto-previous-section
-        (kbd "c") 'magit-commit
-        (kbd "s") 'magit-stage-item
-        (kbd "u") 'magit-unstage-item
-        (kbd "p") 'magit-toggle-section
-        (kbd "P") 'magit-push)
-      (evil-define-key 'normal magit-log-mode-map
-        (kbd "j") 'magit-goto-next-section
-        (kbd "k") 'magit-goto-previous-section)
-      (evil-define-key 'normal magit-diff-mode-map
-        (kbd "j") 'magit-goto-next-section
-        (kbd "k") 'magit-goto-previous-section)
-      (evil-define-key 'normal magit-process-mode-map
-        (kbd "j") 'evil-next-line
-        (kbd "k") 'evil-previous-line))))
+       (evil-set-initial-state 'magit-mode 'normal)
+       (evil-set-initial-state 'magit-status-mode 'normal)
+       (evil-set-initial-state 'magit-diff-mode 'normal)
+       (evil-set-initial-state 'magit-log-mode 'normal)
+       (evil-set-initial-state 'magit-process-mode 'normal)
+       (evil-define-key 'normal magit-mode-map
+         (kbd "-") 'magit-diff-smaller-hunks
+         (kbd "+") 'magit-diff-larger-hunks
+         (kbd "j") 'magit-goto-next-section
+         (kbd "k") 'magit-goto-previous-section
+         (kbd "c") 'magit-commit
+         (kbd "s") 'magit-stage-item
+         (kbd "u") 'magit-unstage-item
+         (kbd "p") 'magit-toggle-section
+         (kbd "P") 'magit-push
+         (kbd "r") 'magit-refresh)
+
+       (evil-define-key 'normal magit-log-mode-map
+         (kbd "j") 'magit-goto-next-section
+         (kbd "k") 'magit-goto-previous-section)
+
+       (evil-define-key 'normal magit-diff-mode-map
+         (kbd "j") 'magit-goto-next-section
+         (kbd "k") 'magit-goto-previous-section)
+
+       (evil-define-key 'normal magit-process-mode-map
+         (kbd "j") 'evil-next-line
+         (kbd "k") 'evil-previous-line))))
 (provide 'keymaps/magit)
 ;;; keymaps/magit.el ends here
