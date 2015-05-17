5947a5fe857449fd7ea25345ff4c52deea63eab3
DJP <djpade@gmail.com>
2015-05-16 22:44:49 -0400

    Added flx fuzzy matching to ido

--- config/plug/ido.el
+++ config/plug/ido.el
@@ -2,8 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 (use-package ido
+  :ensure t
+  :ensure flx-ido
   :demand evil
-  :init (ido-mode t)
+  :init
+  (progn
+    (ido-mode t)
+    (ido-everywhere t)
+    (flx-ido-mode t)
+    ;; disable ido faces to see flx highlights
+    (setq ido-enable-flex-matching t)
+    (setq ido-use-faces nil))
+  ;; To disable flx highlights use this:
+  ;; (setq flx-ido-use-faces nil)
   :config
   (add-hook 'ido-minibuffer-setup-hook
             'keymaps/ido))
