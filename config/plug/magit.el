fdbc989626a1194aa6c35275735a4c5c46a178b7
DJP <djpade@gmail.com>
2015-05-17 12:01:44 -0400

    Added ido completion to magit (? don't really know what it does)

--- config/plug/magit.el
+++ config/plug/magit.el
@@ -8,9 +8,11 @@
   :demand magit
   :demand magit-gitflow
   :demand evil
+  :demand ido
   :config
   (setq magit-auto-revert-mode nil)
   (setq magit-last-seen-setup-instructions "1.4.0")
+  (setq magit-completing-read-function 'magit-ido-completing-read)
   ;;(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
   )
 
