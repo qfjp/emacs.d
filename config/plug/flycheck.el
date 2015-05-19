1ada07a2533ae4bc100b15f695f7f2d93a9fc419
DJP <djpade@gmail.com>
2015-05-19 17:13:46 -0400

    Flyspell-prog-mode causes emacsclient to hang on first init, so i removed it

--- config/plug/flycheck.el
+++ config/plug/flycheck.el
@@ -15,8 +15,11 @@
 (defun activate-flyspell ()
   "Force flyspell mode."
   (flyspell-mode t))
+(custom-set-variables
+ '(ispell-dictionary "en_US")
+ '(ispell-program-name "/usr/bin/aspell"))
 (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;(add-hook 'prog-mode-hook 'flyspell-prog-mode)
 (add-hook 'org-mode-hook 'activate-flyspell)
 (add-hook 'text-mode-hook 'activate-flyspell)
 (add-hook 'message-mode-hook 'activate-flyspell)
