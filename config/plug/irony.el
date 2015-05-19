7c68765e2fbe7afd8d68489c5c461c7f37171fc9
DJP <djpade@gmail.com>
2015-05-19 00:09:02 -0400

    Added flycheck and eldoc support to irony mode

--- config/plug/irony.el
+++ config/plug/irony.el
@@ -4,6 +4,8 @@
 (use-package irony
   :ensure t
   :ensure company-irony
+  :ensure flycheck-irony
+  :ensure irony-eldoc
   :init
   (progn
     (add-hook 'c++-mode-hook 'irony-mode)
@@ -12,7 +14,15 @@
   :config
   (eval-after-load 'company
     '(add-to-list 'company-backends 'company-irony))
-  (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands))
+  (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
+  ;; flycheck
+  (eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+  ;; eldoc
+  (eval-after-load 'irony
+    '(add-hook 'irony-mode-hook #'irony-eldoc)))
 
 (provide 'plug/irony)
 ;;; irony.el ends here
