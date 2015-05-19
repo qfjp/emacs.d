2c24eeb3279c3b6e03d89c606ef0b620c277d13d
DJP <djpade@gmail.com>
2015-05-19 14:29:47 -0400

    Added flyspell to flycheck plugin

--- config/plug/flycheck.el
+++ config/plug/flycheck.el
@@ -11,5 +11,15 @@
   (add-hook 'python-mode-hook
             (lambda ()
               (flycheck-select-checker 'python-pylint))))
+
+(defun activate-flyspell ()
+  "Force flyspell mode."
+  (flyspell-mode t))
+(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(add-hook 'org-mode-hook 'activate-flyspell)
+(add-hook 'text-mode-hook 'activate-flyspell)
+(add-hook 'message-mode-hook 'activate-flyspell)
+
 (provide 'plug/flycheck)
-;;; plug/flycheck.el ends here
+;;; flycheck.el ends here
