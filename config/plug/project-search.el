987746f1b33f755eb978520c3549c76a7b3b14c0
DJP <djpade@gmail.com>
2015-05-20 00:58:27 -0400

    Added sr-speedbar to emacs

--- config/plug/project-search.el
+++ config/plug/project-search.el
@@ -6,6 +6,13 @@
   :init
   (projectile-global-mode))
 
+(use-package sr-speedbar
+  :ensure t
+  :config
+  (setq sr-speedbar-right-side nil)
+  (add-hook 'speedbar-mode-hook #'(lambda () (visual-line-mode -1)))
+  )
+
 ;; (use-package project-explorer
 ;;   :ensure t)
 (add-to-list 'load-path (concat user-emacs-directory "config/plug/project-explorer"))
