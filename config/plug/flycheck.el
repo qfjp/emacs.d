e38b6ac6a0aeb037f865b9212bf69d81fbeef970
DJP <djpade@gmail.com>
2015-05-22 10:18:29 -0400

    Changed sh-mode flycheck checker to shellcheck, forced compatibility with zsh

--- config/plug/flycheck.el
+++ config/plug/flycheck.el
@@ -8,12 +8,13 @@
   :init
   (global-flycheck-mode t)
   :config
-  (setq flycheck-highlighting-mode 'lines)
+  ;(setq flycheck-highlighting-mode 'lines)
   (add-hook 'after-init-hook #'global-flycheck-mode)
   (add-hook 'python-mode-hook
             (lambda ()
               (flycheck-select-checker 'python-pylint)))
-  (add-hook 'sh-mode-hook #'(lambda () (flycheck-select-checker 'sh-shellcheck))))
+  (add-hook 'sh-mode-hook #'(lambda () (flycheck-select-checker 'sh-shellcheck)))
+  (add-to-list 'flycheck-shellcheck-supported-shells 'zsh))
 
 (defun activate-flyspell ()
   "Force flyspell mode."
@@ -26,6 +27,25 @@
 (add-hook 'org-mode-hook 'activate-flyspell)
 (add-hook 'text-mode-hook 'activate-flyspell)
 (add-hook 'message-mode-hook 'activate-flyspell)
+
+
+;; Shellcheck zsh fix
+(flycheck-define-checker sh-shellcheck
+  "A shell script syntax and style checker using Shellcheck.
+
+See URL `https://github.com/koalaman/shellcheck/'."
+  :command ("shellcheck"
+            "--format" "checkstyle"
+            "--shell" (eval (if (eq sh-shell 'zsh)
+                                (symbol-name 'bash)
+                              (symbol-name sh-shell)))
+            (option "--exclude" flycheck-shellcheck-excluded-warnings list
+                    flycheck-option-comma-separated-list)
+            source)
+  :error-parser flycheck-parse-checkstyle
+  :error-filter flycheck-dequalify-error-ids
+  :modes sh-mode
+  :predicate (lambda () (memq sh-shell flycheck-shellcheck-supported-shells)))
 
 (provide 'plug/flycheck)
 ;;; flycheck.el ends here
